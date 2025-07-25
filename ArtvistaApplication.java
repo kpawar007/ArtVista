361f3beaf98ac543796ef7b88aa21faabed91c62
Kanhaiya Pawar <124275072+kpawar007@users.noreply.github.com>
2025-07-25 10:01:29 +0530

    Add files via upload

---
+++ ArtvistaApplication.java
@@ -0,0 +1,14 @@
+package com.artapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtvistaApplication.class, args);
+	}
+
+}
